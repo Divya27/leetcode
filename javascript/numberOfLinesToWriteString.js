da238209d797d80738bf9685c46265ec959e67bd
Divya Ahuja <divya.ahuja@jump360.me>
2022-07-12 20:51:37 +0530

    new added

--- javascript/numberOfLinesToWriteString.js
+++ javascript/numberOfLinesToWriteString.js
@@ -8,21 +8,28 @@
 var numberOfLines = function (widths, s) {
   let lines = 0
   const maxPixels = 100
+  const base = 'a'.charCodeAt(0)
+  // console.log("base: ", base)
   let currentPixels = 0
   for (let i = 0; i < s.length; i++) {
-    currentPixels += widths[i]
-    if (currentPixels === maxPixels) {
-      lines += 1
-      currentPixels = 0
-    }
+    const width = widths[s.charCodeAt(i) - base]
+    currentPixels += width
+    // if (currentPixels === maxPixels) {
+    //   lines += 1
+    //   currentPixels = 0
+    // }
     if (currentPixels > maxPixels) {
-      currentPixels = widths[i]
+      currentPixels = width
       lines += 1
     }
+    console.log("currentPixels: ", currentPixels)
   }
   // console.log("lines:", lines)
-  return (currentPixels > 0) ? lines + 1 : lines
+  return (currentPixels > 0) ? [lines + 1, currentPixels] : [lines, currentPixels]
 };
 
 // console.log(numberOfLines([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10], "abcdefghijklmnopqrstuvwxyz"))
+// console.log(numberOfLines([4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10], "bbbcccdddaaa"))
+
 console.log(numberOfLines([4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10], "bbbcccdddaaa"))
+// console.log(numberOfLines([3, 4, 10, 4, 8, 7, 3, 3, 4, 9, 8, 2, 9, 6, 2, 8, 4, 9, 9, 10, 2, 4, 9, 10, 8, 2], "mqblbtpvicqhbrejb"))
