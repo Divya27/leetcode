5ad5aafbf70f50d3caac43c8bb16f737de111b42
Divya Ahuja <divya.ahuja@jump360.me>
2022-07-13 17:57:06 +0530

    unique email addresses

--- javascript/uniqueEmailAddresses.js
+++ javascript/uniqueEmailAddresses.js
@@ -5,10 +5,9 @@
  * https://leetcode.com/problems/unique-email-addresses/
  */
 var numUniqueEmails = function (emails) {
-  let count = 0
-  // for (let email of emails) {
-
-  // }
+  return new Set(emails.map(email => `${email.split('@')[0].split('+')[0].split('.').join('')}@${email.split('@')[1]}`)).size
 };
 
-console.log(numUniqueEmails(["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]))
+// console.log(numUniqueEmails(["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]))
+// console.log(numUniqueEmails(["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"]))
+console.log(numUniqueEmails(["fg.r.u.uzj+o.pw@kziczvh.com", "r.cyo.g+d.h+b.ja@tgsg.z.com", "fg.r.u.uzj+o.f.d@kziczvh.com", "r.cyo.g+ng.r.iq@tgsg.z.com", "fg.r.u.uzj+lp.k@kziczvh.com", "r.cyo.g+n.h.e+n.g@tgsg.z.com", "fg.r.u.uzj+k+p.j@kziczvh.com", "fg.r.u.uzj+w.y+b@kziczvh.com", "r.cyo.g+x+d.c+f.t@tgsg.z.com", "r.cyo.g+x+t.y.l.i@tgsg.z.com", "r.cyo.g+brxxi@tgsg.z.com", "r.cyo.g+z+dr.k.u@tgsg.z.com", "r.cyo.g+d+l.c.n+g@tgsg.z.com", "fg.r.u.uzj+vq.o@kziczvh.com", "fg.r.u.uzj+uzq@kziczvh.com", "fg.r.u.uzj+mvz@kziczvh.com", "fg.r.u.uzj+taj@kziczvh.com", "fg.r.u.uzj+fek@kziczvh.com"]))
