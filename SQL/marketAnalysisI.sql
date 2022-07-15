22b8623273087cdd3b932b2f8cc821e500505c79
Divya Ahuja <divya.ahuja@jump360.me>
2022-07-15 17:47:56 +0530

    new added

--- SQL/marketAnalysisI.sql
+++ SQL/marketAnalysisI.sql
@@ -1,1 +1,11 @@
 # https://leetcode.com/problems/market-analysis-i
+
+# Write your MySQL query statement below
+
+SELECT 
+Users.user_id AS buyer_id , Users.join_date,
+IFNULL(buy_count.orders_in_2019, 0) AS orders_in_2019 FROM
+Users 
+LEFT JOIN
+(SELECT buyer_id, COUNT(buyer_id) AS orders_in_2019 FROM
+Orders WHERE order_date LIKE '%2019%' GROUP BY buyer_id)  AS buy_count ON (Users.user_id = buy_count.buyer_id)
--- javascript/monotonicArray.js
+++ javascript/monotonicArray.js
@@ -6,4 +6,16 @@
  */
 var isMonotonic = function (nums) {
 
+  let isIncreasing = false
+  let isDecreasing = false
+
+  let i = 1
+  while (nums[i] !== nums[0]) {
+    if (nums[i] > nums[0]) isIncreasing = true
+    else if (nums[i] < nums[0]) isDecreasing = true
+    i += 1
+  }
+  console.log("i:", i, isDecreasing, isIncreasing)
 };
+
+console.log(isMonotonic([1, 1, 2, 2, 3]))
