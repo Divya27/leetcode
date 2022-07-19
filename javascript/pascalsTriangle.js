081b85bfe3dc89e54d5d53dd81d30eed661a6802
Divya Ahuja <divya.ahuja@jump360.me>
2022-07-19 19:46:13 +0530

    new added

---
+++ javascript/pascalsTriangle.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ * 
+ * https://leetcode.com/problems/pascals-triangle/
+ */
+var generate = function (numRows) {
+  var pascal = [];
+  for (var i = 0; i < numRows; i++) {
+    pascal[i] = [];
+    pascal[i][0] = 1;
+    for (var j = 1; j < i; j++) {
+      pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+    }
+    pascal[i][i] = 1;
+  }
+  return pascal;
+};
+
+console.log(generate(5))
