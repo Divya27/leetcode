dda0cf3b19efdaa3b2604456f361e714ca7ab75a
Divya Ahuja <divya.ahuja@jump360.me>
2022-08-07 21:24:43 +0530

    new added

---
+++ javascript/base7.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {string}
+ * 
+ * https://leetcode.com/problems/base-7/
+ */
+var convertToBase7 = function (num) {
+  return num.toString(7)
+};
+
+console.log(convertToBase7(100))
