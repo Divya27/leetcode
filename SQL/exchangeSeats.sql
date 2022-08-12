b6a06abf11c7ef51aae3830b1282d4ce581263fa
Divya Ahuja <divya.ahuja@jump360.me>
2022-08-12 18:23:54 +0530

    new added

--- SQL/exchangeSeats.sql
+++ SQL/exchangeSeats.sql
@@ -1,1 +1,11 @@
 # https://leetcode.com/problems/exchange-seats
+
+# Write your MySQL query statement below
+SELECT
+	CASE
+		WHEN id % 2 != 0 AND id = (SELECT COUNT(*) FROM seat) THEN id
+		WHEN id % 2 = 0 THEN id - 1
+		ELSE id + 1
+	END as id, student 
+FROM seat
+ORDER BY id
