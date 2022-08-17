5f25660ce081f154af777e77dea3294d073bd3e7
Divya Ahuja <divya.ahuja@jump360.me>
2022-08-17 21:51:01 +0530

    new added

--- javascript/uniqueMorseCodeWords.js
+++ javascript/uniqueMorseCodeWords.js
@@ -5,6 +5,13 @@
  * https://leetcode.com/problems/unique-morse-code-words/
  */
 var uniqueMorseRepresentations = function (words) {
-  const charCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
-  
+  const alphabetCode = {
+    a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.', f: '..-.', g: '--.', h: '....', i: '..', j: '.---', k: '-.-', l: '.-..', m: '--',
+    n: '-.', o: '---', p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-', u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..'
+  }
+
+  return new Set(words.map(word => word.split("").map(letter => alphabetCode[letter]).join(""))).size
+
 };
+
+console.log(uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]))
